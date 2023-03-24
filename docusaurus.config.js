1f70e02c6413b836ea10de06f11b58b21fed7827
Sirio Jansen-Sanchez <sjcoolguy101@gmail.com>
2023-03-24 00:38:55 -0400

    Added March 4 test-log

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -122,6 +122,10 @@ const config = {
         respectPrefersColorScheme: false,
       }
     }),
+    markdown: {
+      mermaid: true,
+    },
+    themes: ['@docusaurus/theme-mermaid'],
 };
 
 module.exports = config;
